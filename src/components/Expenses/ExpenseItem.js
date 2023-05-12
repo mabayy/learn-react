f811d608cbb306561bce2b94e50fd6f97bdbc71f
Mahesha Bayu <bayupaksi2007@gmail.com>
2023-05-12 14:06:54 +0700

    update 3
    *tambah form input

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,25 @@
+import React, { useState } from "react";
+
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = (data) => {
+  const [title, setTitle] = useState(data.title);
+
+  const clickHandler = () => {
+    setTitle("Terupdate!");
+    console.log(title);
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={data.date} />
       <div className="expense-item__description">
-        <h2>{data.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">Rp.{data.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
